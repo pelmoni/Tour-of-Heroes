92c24e2faeb4960a062329828730a28a4ecb0b3b
pelmoni <monika.unruh511@gmail.com>
2024-01-03 00:49:58 +0100

    navigation complete (step 5)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
-import { HeroesComponent} from "./heroes/heroes.components";
+import { HeroesComponent} from "./heroes/heroes.component";
 import {RouterModule, Routes} from "@angular/router";
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {HeroDetailComponent} from "./hero-detail/hero-detail.component";
 
 const routes:Routes=[
   {path: '', redirectTo: '/dashboard', pathMatch:'full'},
   {path: 'heroes', component:HeroesComponent},
-  {path: 'dashboard', component:DashboardComponent}
+  {path: 'dashboard', component:DashboardComponent},
+  {path: 'detail/:id', component:HeroDetailComponent}
 ]
 
 @NgModule({
--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,17 +1,34 @@
 import { Component, Input } from '@angular/core';
 import {Hero} from "../hero";
-import {NgIf, UpperCasePipe} from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import {ActivatedRoute} from "@angular/router";
+import {Location} from "@angular/common";
+import {HeroService} from "../hero.service";
 
 @Component({
-  standalone: true,
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css'],
-  imports: [FormsModule, NgIf, UpperCasePipe],
 })
 
 
 export class HeroDetailComponent {
   @Input() hero?: Hero;
+
+  constructor(private route:ActivatedRoute,
+              private heroService:HeroService,
+              private location:Location) {
+  }
+
+  ngOnInit():void{
+    this.getHero();
+  }
+
+  getHero():void{
+    const id=Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe(hero=>this.hero=hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
