45e794cd87ca84fc24b83d430aaef42b2af57e59
pelmoni <monika.unruh511@gmail.com>
2024-01-16 17:39:33 +0100

    added darkmode toggle

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,28 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+const root = document.documentElement;
+
+// Get the toggle button
+const toggle = document.getElementById("toggle");
+// Get the user's preference from localStorage
+const darkMode = localStorage.getItem("dark-mode");
+// Check if the user has already chosen a theme
+if (darkMode) {
+  // If yes, apply it to the root element
+  root.classList.add("dark-theme");
+}
+// Add an event listener to the toggle button
+if (toggle != null){
+toggle.addEventListener("click", () => {
+  // Toggle the dark-theme class on the root element
+  root.classList.toggle("dark-theme");
+  // Store or remove the user's preference in localStorage
+  if (root.classList.contains("dark-theme")) {
+    localStorage.setItem("dark-mode", "true");
+  } else {
+    localStorage.removeItem("dark-mode");
+  }
+});
+}
